67f94d1bff475878e70238a29a86e6745626d6c9
Velin Dankov <velin80@web.de>
2025-06-08 18:03:13 +0300

    Refactor user model to use bcrypt for password hashing and update package dependencies

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,6 +1,20 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
 
-const userSchema = new Schema();
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 14);
+});
 
 const User = model("User", userSchema);
 
